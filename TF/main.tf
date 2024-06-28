27b4796effd16ac4a92fb71681d8a57769e8db86
MaheswaranG <gmaheswaran06@gmail.com>
2024-06-28 22:46:17 +0530

    test deploymnet

--- TF/main.tf
+++ TF/main.tf
@@ -3,3 +3,12 @@ resource "aws_ssm_parameter" "test"{
     type = "String"
     value= "test1"
 }
+
+resource "aws_s3_bucket" "example" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "test-bucket-oidc-1234"
+    Environment = "Dev"
+  }
+}
