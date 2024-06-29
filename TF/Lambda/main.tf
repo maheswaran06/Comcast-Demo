685b363aec2dad784edb92a32d3e54a58e7c7102
MaheswaranG <gmaheswaran06@gmail.com>
2024-06-29 11:21:04 +0530

    lambda deployment

--- TF/Lambda/main.tf
+++ TF/Lambda/main.tf
@@ -1,7 +1,6 @@
 ########## LAMBDA FUNCTION CODE #################
 resource "aws_lambda_function" "demo_lambda" {
-  # If the file is not in the current working directory you will need to include a
-  # path.module in the filename.
+  
   filename      = var.filename
   function_name = var.function_name
   role          = var.role
--- TF/main.tf
+++ TF/main.tf
@@ -1,50 +1,50 @@
-#### IAM POLICY ####
+# #### IAM POLICY ####
 
-data "aws_iam_policy_document" "assume_role" {
-  statement {
-    effect = "Allow"
+# data "aws_iam_policy_document" "assume_role" {
+#   statement {
+#     effect = "Allow"
 
-    principals {
-      type        = "Service"
-      identifiers = ["lambda.amazonaws.com"]
-    }
-
-    actions = ["sts:AssumeRole"]
-  }
-}
-
-#### IAM ROLE ###
-
-module "comcast_iam_role" {
-  source    = "./IAM_Role"
-  name               = "Comcast-Demo-lambda-policy"
-  assume_role_policy = data.aws_iam_policy_document.assume_role.json
-}
-
-#### LAMBDA CODE ###
-
-data "archive_file" "lambda" {
-  type        = "zip"
-  source_file = "../Lambda/lambda.py"
-  output_path = "lambda_function_payload.zip"
-}
-
-### LAMBDA FUNCTION ###
-
-module "comcast_lambda_function" {
-  source        = "./Lambda"
-  filename      = "lambda_function_payload.zip"
-  function_name = "Comcast-Demo-lambda-function"
-  role          = module.comcast_iam_role.role_arn
-  handler       = "index.test"
-
-  source_code_hash = data.archive_file.lambda.output_base64sha256
-
-  runtime = "python3.12"
-
-#   environment {
-#     variables = {
-#       env = "dev"
+#     principals {
+#       type        = "Service"
+#       identifiers = ["lambda.amazonaws.com"]
 #     }
+
+#     actions = ["sts:AssumeRole"]
 #   }
-}
+# }
+
+# #### IAM ROLE ###
+
+# module "comcast_iam_role" {
+#   source    = "./IAM_Role"
+#   name               = "Comcast-Demo-lambda-policy"
+#   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+# }
+
+# #### LAMBDA CODE ###
+
+# data "archive_file" "lambda" {
+#   type        = "zip"
+#   source_file = "../Lambda/lambda.py"
+#   output_path = "lambda_function_payload.zip"
+# }
+
+# ### LAMBDA FUNCTION ###
+
+# module "comcast_lambda_function" {
+#   source        = "./Lambda"
+#   filename      = "lambda_function_payload.zip"
+#   function_name = "Comcast-Demo-lambda-function"
+#   role          = module.comcast_iam_role.role_arn
+#   handler       = "index.test"
+
+#   source_code_hash = data.archive_file.lambda.output_base64sha256
+
+#   runtime = "python3.12"
+
+# #   environment {
+# #     variables = {
+# #       env = "dev"
+# #     }
+# #   }
+# }
