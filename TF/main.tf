babe1a3c609e06ccde9e1b5119e7129526dd346e
MaheswaranG <gmaheswaran06@gmail.com>
2024-06-29 08:30:33 +0530

    lambda deployment

--- TF/main.tf
+++ TF/main.tf
@@ -29,7 +29,7 @@ resource "aws_iam_role" "demo_lambda" {
 
 data "archive_file" "lambda" {
   type        = "zip"
-  source_file = "../Lambda"
+  source_file = "../Lambda/"
   output_path = "lambda_function_payload.zip"
 }
 
