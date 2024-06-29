db685a3bbba5150d113ae6f63a6232a1d6a3e73c
MaheswaranG <gmaheswaran06@gmail.com>
2024-06-29 14:54:46 +0530

    Deployment of Lambda code

--- TF/S3/outputs.tf
+++ TF/S3/outputs.tf
@@ -3,7 +3,7 @@ output "bucket_id" {
   value       = aws_s3_bucket.demo_bucket.id
 }
 
-output "bucket_name" {
+output "bucket_arn" {
   description = "The S3 bucket name"
   value       = aws_s3_bucket.demo_bucket.arn
 }
